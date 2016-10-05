125dec20447b7295f05ed3fab6d5beb8a61397f5
marcomow <marcomow@gmail.com>
2016-10-05 16:52:49 +0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -6,3 +6,8 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+    $(document).onCommaKeyDown(launch, $.easyKey.options.withControlPressed);
+    function launch(){
+  window.open('https://docs.google.com/document/d/PAGEID/edit')
+}
